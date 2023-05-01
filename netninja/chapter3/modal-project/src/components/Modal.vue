c04285ec8d5f60e7332e21a063f93082cbe10436
Frklcedo <franklinmacedo5115@gmail.com>
2023-05-01 02:09:22 -0300

    Multiple components

---
+++ netninja/chapter3/modal-project/src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="blackdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+ .modal{
+     max-width: 400px;
+     padding: 20px;
+     margin: 100px auto;
+     background: #fff;
+     border-radius: 10px;
+ }
+
+ .blackdrop{
+     top: 0;
+     position: fixed;
+     background: rgba( 0,0,0,0.5 );
+     width: 100%;
+     height: 100%;
+ }
+</style>
