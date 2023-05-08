9d365b2f9a54d90b58ed588a0fae6d88f26c0da4
Frklcedo <franklinmacedo5115@gmail.com>
2023-05-07 22:33:08 -0300

    First steps in reaction-timer project

---
+++ netninja/chapter4/reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<script>
+ export default {
+     props: ['delay']
+ }
+</script>
+
+<template>
+    <div class="block">
+        <span>click me</span>
+    </div>
+</template>
+
+<style>
+ .block{
+     max-height: 400px;
+     border-radius: 20px;
+     background-color: #0faf87;
+     color: white;
+     text-align: center;
+     padding: 100px 0;
+     margin: 40px auto;
+ }
+</style>
