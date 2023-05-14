bf46173d7093906401d89c6e7b0595aa5c2bc03d
Frklcedo <franklinmacedo5115@gmail.com>
2023-05-14 17:09:37 -0300

    Removing skills

--- netninja/chapter5/web-form/src/components/SignupForm.vue
+++ netninja/chapter5/web-form/src/components/SignupForm.vue
@@ -15,13 +15,14 @@
         <label>Skills: </label>
         <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
         <div v-for="skill in skills" :key="skill" class="pill">
-            {{ skill }}
+            <span @click="deleteSkill(skill)">{{ skill }}</span>
         </div>
 
         <div class="terms">
             <input type="checkbox" required v-model="terms" />
             <label >Accept terms and conditions</label>
         </div>
+
 
         <!-- <div>
              <input type="checkbox" value="shaun" v-model="names" />
@@ -65,6 +66,9 @@
                  }
                  this.tempSkill = ''
              }
+         },
+         deleteSkill(skill){
+             this.skills = this.skills.filter(item => item !== skill)
          }
      }
  }
@@ -104,5 +108,17 @@
      margin: 0 10px 0 0;
      position: relative;
      top: 2px;
+ }
+ .pill{
+     display: inline-block;
+     margin: 20px 10px 0 0 ;
+     padding: 6px 12px;
+     background: #eee;
+     border-radius: 20px;
+     font-size: 12px;
+     letter-spacing: 1px;
+     font-weight: bold;
+     color: #777;
+     cursor: pointer;
  }
 </style>
