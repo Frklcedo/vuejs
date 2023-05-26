ed2181c4046e8672b112747319c4fa900fb2c9d5
Frklcedo <franklinmacedo5115@gmail.com>
2023-05-25 21:44:08 -0300

    Vue Routing and routing links

---
+++ netninja/chapter6/ninja-jobs/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque dolor nulla, sed vestibulum turpis dictum eget. Fusce eu euismod erat, id vestibulum ligula. Phasellus in erat a mi egestas feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam auctor quis nisi rhoncus gravida. Phasellus vitae sapien nunc. In ut enim nulla. Duis at est nec velit molestie pharetra volutpat quis tortor. Suspendisse sodales vel magna vitae accumsan. Morbi magna nulla, sagittis vel euismod et, sagittis nec dolor. Proin pulvinar tempus rutrum. Quisque bibendum ante et justo dignissim sagittis. Nulla laoreet venenatis feugiat. Nunc.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque dolor nulla, sed vestibulum turpis dictum eget. Fusce eu euismod erat, id vestibulum ligula. Phasellus in erat a mi egestas feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam auctor quis nisi rhoncus gravida. Phasellus vitae sapien nunc. In ut enim nulla. Duis at est nec velit molestie pharetra volutpat quis tortor. Suspendisse sodales vel magna vitae accumsan. Morbi magna nulla, sagittis vel euismod et, sagittis nec dolor. Proin pulvinar tempus rutrum. Quisque bibendum ante et justo dignissim sagittis. Nulla laoreet venenatis feugiat. Nunc.</p>
+  </div>
+</template>
