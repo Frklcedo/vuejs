97f23d649db3e6d0d9f7255ca430f4a10adf3750
Frklcedo <franklinmacedo5115@gmail.com>
2023-05-25 22:53:56 -0300

    Redirect and 404 pages

--- netninja/chapter6/ninja-jobs/src/router/index.js
+++ netninja/chapter6/ninja-jobs/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
 import Jobs from '../views/Jobs/Jobs.vue'
 import JobDetails from '../views/Jobs/JobDetails.vue'
 
@@ -26,6 +27,15 @@ const routes = [
     name: 'jobDetails',
     component: JobDetails,
     props: true
+  },
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
